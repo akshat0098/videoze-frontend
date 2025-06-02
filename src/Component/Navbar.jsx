d61204a7946df480d0c9af4730288ca75bf78bef
Akshat Srivastava <57599972+akshat0098@users.noreply.github.com>
2025-06-02 14:13:34 +0530

    Update Navbar.jsx

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -85,13 +85,13 @@
                 Contact Us
               </Link>
               <Link
-                to="#"
+                to="https://app.videoze.com/sign-up"
                 className="block bg-gradient-to-r from-indigo-600 to-purple-500 text-white px-4 py-2 rounded-full text-sm font-semibold text-center"
               >
                 SignUp
               </Link>
               <Link
-                to="#"
+                to="https://app.videoze.com/sign-in"
                 className="block border border-indigo-400 text-indigo-600 px-4 py-2 rounded-full text-sm font-semibold text-center"
               >
                 Login
